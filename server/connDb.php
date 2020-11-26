ef769109d1f9552de1f1096fbb7ae9f9fed6a249
tonalmathew <tonalmathew1999@gmail.com>
2020-11-26 19:15:46 +0530

    [signup]: :dizzy: updated signup page

--- server/connDb.php
+++ server/connDb.php
@@ -1,5 +1,5 @@
 <?php
-    $conn = new mysqli("localhost","root","","my-notes");
+    $conn = new mysqli("localhost","root","","my_notes");
     if($conn->connect_error){
         die("connection failed!!".$conn->connect_error);
     }
@@ -7,18 +7,62 @@
     $result = array('error'=>false);
     $action = '';
 
-    if(isset($_GET['acton'])){
+    if(isset($_GET['action'])){
         $action = $_GET['action'];
     }
 
     if($action == 'read'){
-        $sql = $conn->query("SELECT * FROM user");
-        $user = array();
+        $sql = $conn->query("SELECT * FROM users");
+        $users = array();
         while($row = $sql->fetch_assoc()){
-            array_push($user, $row);
+            array_push($users, $row);
         }
-        $result['user'] = $user;
+        $result['users'] = $users;
     }
 
+    if($action == 'create'){
+        $name = $_POST['name'];
+        $email = $_POST['email'];
+        $password = $_POST['password'];
+        $sql = $conn->query("INSERT INTO users (name,email,password) VALUES('$name','$email','$password')");
+        
+        if($sql){
+            $result['message'] = "User added sucessfully!";
+        }
+        else{
+            $result['error'] = true;
+            $result['message'] = "Failed to add user!";
+        }
+    }
+
+    if($action == 'update'){
+        $id = $_POST['id'];
+        $name = $_POST['name'];
+        $email = $_POST['email'];
+        $password = $_POST['password'];
+        $sql = $conn->query("UPDATE users SET name='$name' ,email='$email' ,password='$password' WHERE id='$id'");
+        
+        if($sql){
+            $result['message'] = "User updated sucessfully!";
+        }
+        else{
+            $result['error'] = true;
+            $result['message'] = "Failed to update user!";
+        }
+    }
+
+    if($action == 'delete'){
+        $id = $_POST['id'];
+        $sql = $conn->query("DELETE FROM users WHERE id='$id'");
+        
+        if($sql){
+            $result['message'] = "User deleted sucessfully!";
+        }
+        else{
+            $result['error'] = true;
+            $result['message'] = "Failed to delete user!";
+        }
+    }
+    $conn->close();
     echo json_encode($result);
 ?>
